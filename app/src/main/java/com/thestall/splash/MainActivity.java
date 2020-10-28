8f50766ee8cb275c1c514ed30433dca8c8170e76
n01318477 <n01318477@humbermail.ca>
2020-10-28 14:49:53 -0400

    Got rid of splash title started work on sign in page

--- app/src/main/java/com/thestall/splash/MainActivity.java
+++ app/src/main/java/com/thestall/splash/MainActivity.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //remove top bar with title
+        getSupportActionBar().hide();
 
         //loading splash screen
         SplashView splashView = new SplashView(this);
--- app/src/main/java/com/thestall/splash/SignInActivity.java
+++ app/src/main/java/com/thestall/splash/SignInActivity.java
@@ -2,7 +2,9 @@ package com.thestall.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Canvas;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        View background = findViewById(R.id.signInActivity);
+        background.setBackgroundColor(getResources().getColor(R.color.orange));
+
     }
+
 }
