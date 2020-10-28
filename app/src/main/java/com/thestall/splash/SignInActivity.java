734f21b6b38e9e5977ebfecf63d2f8e08e8899e6
n01318477 <n01318477@humbermail.ca>
2020-10-28 15:34:14 -0400

    Finished rough version of SignIn page button needs to be redone, it's poor quality

--- app/src/main/java/com/thestall/splash/SignInActivity.java
+++ app/src/main/java/com/thestall/splash/SignInActivity.java
@@ -11,6 +11,10 @@ public class SignInActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //remove top bar with title
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_sign_in);
 
         View background = findViewById(R.id.signInActivity);
