52b4f7108aee3b2e20bdc72aa5cfe211e983de67
n01318477 <n01318477@humbermail.ca>
2020-12-11 16:46:39 -0500

    added logout button. sends you back to login on success

--- app/src/main/java/com/thestall/splash/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/thestall/splash/ui/notifications/NotificationsFragment.java
@@ -1,9 +1,11 @@
 package com.thestall.splash.ui.notifications;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,11 +14,16 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.thestall.splash.ProfileDetailsActivity;
 import com.thestall.splash.R;
+import com.thestall.splash.SignInActivity;
 
 public class NotificationsFragment extends Fragment {
 
     private NotificationsViewModel notificationsViewModel;
+    private Button logout;
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,6 +37,17 @@ public class NotificationsFragment extends Fragment {
 //                textView.setText(s);
 //            }
 //        });
+
+        logout=(Button)root.findViewById(R.id.logOutButton);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                Intent intent = new Intent(getContext(), SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+
         return root;
     }
 }
